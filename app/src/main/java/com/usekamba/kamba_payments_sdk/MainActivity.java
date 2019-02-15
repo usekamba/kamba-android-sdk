60d2989ac547908b10d0087f30e41d698a887ae7
correia <correiachumbo@gmail.com>
2019-02-15 16:51:55 +0100

    removed configuration keys

--- app/src/main/java/com/usekamba/kamba_payments_sdk/MainActivity.java
+++ app/src/main/java/com/usekamba/kamba_payments_sdk/MainActivity.java
@@ -31,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startPayment = findViewById(R.id.start_payment);
-        ClientConfig.getInstance().configure("8f74aff4-1821-4ad7-9d51-7cf3a303743a",
-                "D4P2HM+35xby+2Ajm0+3FNcD1HF1RGptPp7UqnNgLl8=",
+        ClientConfig.getInstance().configure("",
+                "",
                 ClientConfig.Environment.PRODUCTION);
         checkoutRequest = new CheckoutRequest();
         checkoutRequest.setInitialAmount(90);
