79969585ec7cd3ad1acca538e3dd6c67bdf367f9
Alexandre Juca <corextechnologies@gmail.com>
2019-02-24 14:37:17 +0100

    remove config keys

--- app/src/main/java/com/usekamba/kamba_payments_sdk/PaymentsDemoApplication.java
+++ app/src/main/java/com/usekamba/kamba_payments_sdk/PaymentsDemoApplication.java
@@ -9,8 +9,8 @@ public class PaymentsDemoApplication extends Application {
     public void onCreate() {
         super.onCreate();
         ClientConfig.getInstance()
-                .configure("8f74aff4-1821-4ad7-9d51-7cf3a303743a",
-                        "D4P2HM+35xby+2Ajm0+3FNcD1HF1RGptPp7UqnNgLl8=",
+                .configure("",
+                        "",
                         ClientConfig.Environment.PRODUCTION);
     }
 }
